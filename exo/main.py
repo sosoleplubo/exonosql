a6d6f107013cbb6cfb4a87ac3584de02498f0afb
Sofiane <sofianefugon@icloud.com>
2026-01-14 08:44:47 +0100

    script python OK datajson OK

--- exo/main.py
+++ exo/main.py
@@ -1,1 +1,10 @@
-print("hello")
+import json
+
+with open("data/data1.json", "r") as f1:
+    with open("data/data2.json", "r") as f2:
+        with open("data/data3.json", "w") as f3:
+            data1 = json.load(f1)
+            data2 = json.load(f2)
+            dataFilter = [k for k in data2 if k["remarquable"] == "OUI"]
+            merged_data = [dataFilter, data1]
+            json.dump(merged_data, f3, indent=2)
